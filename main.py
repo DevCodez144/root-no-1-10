f81334c4a88e6c9342c3e21b4ca4ae2442b463a5
DevCodez144 <133110463+DevCodez144@users.noreply.github.com>
2023-07-14 20:48:55 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+for num in range(11):
+    game.splash("The square root of", num)
+    game.splash(num * num)
---
+++ main.ts
@@ -0,0 +1,3 @@
+for (let num = 0; num <= 10; num++) {
+    game.splash("The Sqaure root of " + num + " is:", num * num)
+}
